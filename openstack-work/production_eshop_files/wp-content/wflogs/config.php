df76886ead8534ce62dc83a75bfe45f7f47b320d
Ondrej Špánik <spanik11@gmail.com>
2026-08-07 07:46:49 +0200

    wflogs gitignore

--- openstack-work/production_eshop_files/wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1786551237;s:7:"authKey";s:64:"DBuf42U% KYtg|%Ur|ZFVJ!cwrIk=(sJYy[mt,Ej3_fj8UJrke.e&$6nacXc7RDK";s:7:"version";s:5:"1.1.0";}
